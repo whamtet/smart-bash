0801668c4661d6b25f30def188907d670cfeb1bd
Matthew Molloy <whamtet@gmail.com>
2022-05-19 16:58:54 +0900

    can open files

--- src/clj/smart_bash/parse.clj
+++ src/clj/smart_bash/parse.clj
@@ -3,4 +3,12 @@
 (defn parse []
   [:div
    (for [line (-> "output.txt" slurp (.split "\n"))]
-     [:div line])])
+     (if-let [file (re-find #"/Users.*\.tsx?" line)]
+       (let [[a b] (.split line file)]
+         [:div
+          a
+          [:a {:href "#"
+               :hx-post "root:open"
+               :hx-vals {:file file}} file]
+          b])
+       [:div line]))])
--- src/clj/smart_bash/routes/home.clj
+++ src/clj/smart_bash/routes/home.clj
@@ -1,5 +1,6 @@
 (ns smart-bash.routes.home
   (:require
+    [clojure.java.shell :refer [sh]]
     [ctmx.core :as ctmx]
     [ctmx.render :as render]
     [hiccup.page :refer [html5]]
@@ -20,12 +21,14 @@
      [:body (render/walk-attrs body)]
      [:script {:src "https://unpkg.com/htmx.org@1.5.0"}]))))
 
-(ctmx/defcomponent ^:endpoint root [req ^:long num-clicks]
-  (parse/parse))
+(ctmx/defcomponent ^:endpoint root [req ^:long num-clicks command file]
+  (case command
+    "open" (prn (sh "open" file))
+    (parse/parse)))
 
 (defn home-routes []
   (ctmx/make-routes
    "/"
    (fn [req]
      (html5-response
-      (root req 0)))))
+      (root req)))))
